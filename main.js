56383e1c41d7691d9a25e3db502c97e0fa651e9e
ANA JULIA GHIRALI DA SILVA <u21418@LAPA13.academico.cotuca.unicamp.br>
2022-06-14 17:59:59 -0300

    segundo commit

--- main.js
+++ main.js
@@ -11,9 +11,25 @@ app.use(express.json({
 app.use(cors());
 
 const port = process.env.PORT || 3000;
-
+//exemplo uso do get
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname + '/index.html'))
 });
+
+//POST -> serve para incluir dados no servidor
+app.post('/api', (req, res)=>{
+
+    console.log('foi mandado :\n', req.body)
+    _key = req.body._key;
+    _key2 = req
+
+    res.status(200).send(
+        {sucess: 'true',
+        message: 'Ok correto...'
+    }
+    );
+});
+
+
 
 app.listen(port, () => console.log(`Estou escutando na porta ${port}`));
